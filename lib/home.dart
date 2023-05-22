ba25dfed8f6ecf9481a5b1854e44fded65d54d01
George Mushi <adamprosper99@gmail.com>
2023-05-22 21:46:26 +0300

    updates in all screen

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,8 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sms/flutter_sms.dart';
 import 'package:notifications/call.dart';
 import 'package:notifications/notifications.dart';
-
-
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -14,7 +10,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   String message = "This is a test message!";
   List<String> recipients = ["+255719401594", "+255693331836"];
 
@@ -26,23 +21,33 @@ class _HomeScreenState extends State<HomeScreen> {
     });
     print(_result);
   }
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container( 
-              alignment: Alignment.topCenter,
-              padding: EdgeInsets.all(20),
-              child: Column(
-                children: [
-                      
-                  const SizedBox(
-                    height: 70,
+        child: SingleChildScrollView(
+          child: Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 70,
+                ),
+                Container(
+                  height: 200,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/home.jpg"),
+                    ),
                   ),
-
-                  Row(
+                ),
+                const SizedBox(
+                      height: 20,
+                    ),
+                Row(
                   // Number of columns in the grid
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -50,9 +55,11 @@ class _HomeScreenState extends State<HomeScreen> {
                       onTap: () {
                         // openCamera();
                         Navigator.push(
-                          context, MaterialPageRoute(builder: (context) => CallScreen()));
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => CallScreen()));
                       },
-                      child:  Card(
+                      child: Card(
                         child: Padding(
                           padding: EdgeInsets.symmetric(
                               horizontal: 20.0, vertical: 20),
@@ -114,79 +121,80 @@ class _HomeScreenState extends State<HomeScreen> {
                   height: 70,
                 ),
 
-              //second Row here ************************
-              Row(
-                // Number of columns in the grid
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      // openCamera();
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => NotificationsScreen()));
-                    },
-                    child: Card(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: 20.0, vertical: 20),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.notifications_outlined,
-                              size: 50,
-                            ), // Replace 'icon_name_3' with the desired icon
-                            SizedBox(height: 8),
-                            Text(
-                              'Notifications',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
+                //second Row here ************************
+                Row(
+                  // Number of columns in the grid
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        // openCamera();
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => NotificationsScreen()));
+                      },
+                      child: Card(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 20),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.notifications_outlined,
+                                size: 50,
+                              ), // Replace 'icon_name_3' with the desired icon
+                              SizedBox(height: 8),
+                              Text(
+                                'Notifications',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      // _openGallery();
-                      // _sendSMS(message, recipients);
-                    },
-                    child:Card(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: 20.0, vertical: 20),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.message_outlined,
-                              size: 50,
-                            ), // Replace 'icon_name_3' with the desired icon
-                            SizedBox(height: 8),
-                            Text(
-                              'Message',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-                ],
-              ),
+                    // InkWell(
+                    //   onTap: () {
+                    //     // _openGallery();
+                    //     // _sendSMS(message, recipients);
+                    //   },
+                    //   child:Card(
+                    //     child: Padding(
+                    //       padding: EdgeInsets.symmetric(
+                    //           horizontal: 20.0, vertical: 20),
+                    //       child: Column(
+                    //         mainAxisAlignment: MainAxisAlignment.center,
+                    //         children: [
+                    //           Icon(
+                    //             Icons.message_outlined,
+                    //             size: 50,
+                    //           ), // Replace 'icon_name_3' with the desired icon
+                    //           SizedBox(height: 8),
+                    //           Text(
+                    //             'Message',
+                    //             textAlign: TextAlign.center,
+                    //             style: TextStyle(
+                    //               fontSize: 16,
+                    //               fontWeight: FontWeight.bold,
+                    //             ),
+                    //           ),
+                    //         ],
+                    //       ),
+                    //     ),
+                    //   ),
+                    // ),
+                  ],
+                ),
+              ],
+            ),
           ),
+        ),
       ),
     );
   }
--- lib/notifications.dart
+++ lib/notifications.dart
@@ -10,90 +10,119 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: Center(
         child: Column(
+          
           children: [
-            ElevatedButton(
-              onPressed: () async {
-                bool isallowed =
-                    await AwesomeNotifications().isNotificationAllowed();
-                if (!isallowed) {
-                  //no permission of local notification
-                  AwesomeNotifications().requestPermissionToSendNotifications();
-                } else {
-                  //show notification
-                  AwesomeNotifications().createNotification(
-                      content: NotificationContent(
-                          //simgple notification
-                          id: 123,
-                          channelKey:
-                              'basic', //set configuration wuth key "basic"
-                          title: 'Welcome to FlutterCampus.com',
-                          body: 'This simple notification is from Flutter App',
-                          payload: {"name": "FlutterCampus"}));
-                }
-              },
-              child: Text("Show Notification")),
-          ElevatedButton(
-              onPressed: () async {
-                bool isallowed =
-                    await AwesomeNotifications().isNotificationAllowed();
-                if (!isallowed) {
-                  //no permission of local notification
-                  AwesomeNotifications().requestPermissionToSendNotifications();
-                } else {
-                  //show notification
-                  AwesomeNotifications().createNotification(
-                      content: NotificationContent(
-                          //with asset image
-                          id: 1234,
-                          channelKey: 'image',
-                          title: 'Simple Notification With Image',
-                          body: 'This simple notification is from Flutter App',
-                          bigPicture: 'asset://assets/images/elephant.jpg',
-                          notificationLayout: NotificationLayout.BigPicture,
-                          fullScreenIntent: true, //it will display over app
-                          locked: true,
-                          payload: {"id": "1234"}));
-                }
-              },
-              child: Text("Show Notification With Asset Image")),
-          ElevatedButton(
-              onPressed: () async {
-                bool isallowed =
-                    await AwesomeNotifications().isNotificationAllowed();
-                if (!isallowed) {
-                  //no permission of local notification
-                  AwesomeNotifications().requestPermissionToSendNotifications();
-                } else {
-                  //show notification
-                  AwesomeNotifications().createNotification(
-                      content: NotificationContent(
-                          //with image from URL
-                          id: 12345,
-                          channelKey: 'image',
-                          title: 'Simple Notification with Network Image',
-                          body: 'This simple notification is from Flutter App',
-                          bigPicture:
-                              'https://www.fluttercampus.com/img/logo_small.webp',
-                          notificationLayout: NotificationLayout.BigPicture,
-                          payload: {"id": "12345"}));
-                }
-              },
-              child: Text("Show Notification With Image From URL")),
-          ElevatedButton(
-              onPressed: () {
-                AwesomeNotifications().dismiss(123);
-              },
-              child: Text("Cancel Notification")),
-          ElevatedButton(
-              onPressed: () {
-                AwesomeNotifications().dismissAllNotifications();
-              },
-              child: Text("Cancel All Notifications")),
-                
+
+                SizedBox(height: 100,),
+
+                Container(
+                  height:250 ,
+                  width: double.infinity,
+                  decoration:BoxDecoration(
+                    image: DecorationImage(image: AssetImage("assets/images/notification.jpg"))
+                  ),
+
+                ),
+
+            Container(
+              height:250,
+              width: double.infinity,
+              child: Column(
+              
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+            
+                  ElevatedButton(
+                      onPressed: () async {
+                        bool isallowed =
+                            await AwesomeNotifications().isNotificationAllowed();
+                        if (!isallowed) {
+                          //no permission of local notification
+                          AwesomeNotifications()
+                              .requestPermissionToSendNotifications();
+                        } else {
+                          //show notification
+                          AwesomeNotifications().createNotification(
+                              content: NotificationContent(
+                                  //simgple notification
+                                  id: 123,
+                                  channelKey:
+                                      'basic', //set configuration wuth key "basic"
+                                  title: 'Welcome to FlutterCampus.com',
+                                  body: 'This simple notification is from Flutter App',
+                                  payload: {"name": "FlutterCampus"}));
+                        }
+                      },
+                      child: Text("Notification On")),
+                  ElevatedButton(
+                      onPressed: () async {
+                        bool isallowed =
+                            await AwesomeNotifications().isNotificationAllowed();
+                        if (!isallowed) {
+                          //no permission of local notification
+                          AwesomeNotifications()
+                              .requestPermissionToSendNotifications();
+                        } else {
+                          //show notification
+                          AwesomeNotifications().createNotification(
+                              content: NotificationContent(
+                                  //with asset image
+                                  id: 1234,
+                                  channelKey: 'image',
+                                  title: 'Simple Notification With Image',
+                                  body:
+                                      'This simple notification is from Flutter App',
+                                  bigPicture: 'asset://assets/images/elephant.jpg',
+                                  notificationLayout: NotificationLayout.BigPicture,
+                                  fullScreenIntent: true, //it will display over app
+                                  locked: true,
+                                  payload: {"id": "1234"}));
+                        }
+                      },
+                      child: Text("Display Notification")),
+                  ElevatedButton(
+                      onPressed: () async {
+                        bool isallowed =
+                            await AwesomeNotifications().isNotificationAllowed();
+                        if (!isallowed) {
+                          //no permission of local notification
+                          AwesomeNotifications()
+                              .requestPermissionToSendNotifications();
+                        } else {
+                          //show notification
+                          AwesomeNotifications().createNotification(
+                              content: NotificationContent(
+                                  //with image from URL
+                                  id: 12345,
+                                  channelKey: 'image',
+                                  title: 'Simple Notification with Network Image',
+                                  body:
+                                      'This simple notification is from Flutter App',
+                                  bigPicture: 'https://www.fluttercampus.com/img/logo_small.webp',
+                                  notificationLayout: NotificationLayout.BigPicture,
+                                  payload: {"id": "12345"}));
+                        }
+                      },
+                      child: Text("Url Notifications")),
+                  ElevatedButton(
+                      onPressed: () {
+                        AwesomeNotifications().dismiss(123);
+                      },
+                      child: Text("Clear Notification")),
+                  ElevatedButton(
+                      onPressed: () {
+                        AwesomeNotifications().dismissAllNotifications();
+                      },
+                      child: Text("Clear all Notifications")),
+                ],
+              ),
+            ),
           ],
-        )),
+        ),
+      ),
     );
   }
 }
